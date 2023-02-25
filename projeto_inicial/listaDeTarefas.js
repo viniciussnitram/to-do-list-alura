4b7da61ed85b5970cd6a71846b5f11e19a707e25
Vinicius Martins <vinicius.martins.vsm@outlook.com>
2023-02-25 14:24:34 -0300

    Adding new function, creating a component BotaoConcluir, added class to this component and text

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -10,6 +10,8 @@ const criarTarefa = (evento) => {
     const conteudo = `<p class="content">${valor}</p>`;
 
     tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConcluir());
     lista.appendChild(tarefa);
     input.value = " ";
 }
@@ -17,3 +19,16 @@ const criarTarefa = (evento) => {
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConcluir = () => {
+    const botaoConcluir = document.createElement('button');
+
+    botaoConcluir.classList.add('check-button');
+    botaoConcluir.innerText = 'Concluir';
+
+    botaoConcluir.addEventListener('click', () => {
+        console.log('Fui clicado');
+    });
+
+    return botaoConcluir;
+}
