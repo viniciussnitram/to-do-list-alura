a9c916f74d906aae7b6f182711a1638550448346
Vinicius Martins <vinicius.martins.vsm@outlook.com>
2023-02-25 14:35:04 -0300

    Adding function of JavaScript IIFE

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -1,40 +1,42 @@
-const criarTarefa = (evento) => {
-    evento.preventDefault();
+( () => {
+    const criarTarefa = (evento) => {
+        evento.preventDefault();
 
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+        const valor = input.value;
 
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('task');
+        const conteudo = `<p class="content">${valor}</p>`;
 
-    tarefa.innerHTML = conteudo;
+        tarefa.innerHTML = conteudo;
 
-    tarefa.appendChild(BotaoConcluir());
-    lista.appendChild(tarefa);
-    input.value = " ";
-}
+        tarefa.appendChild(BotaoConcluir());
+        lista.appendChild(tarefa);
+        input.value = " ";
+    }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+    const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener('click', criarTarefa);
+    novaTarefa.addEventListener('click', criarTarefa);
 
-const BotaoConcluir = () => {
-    const botaoConcluir = document.createElement('button');
+    const BotaoConcluir = () => {
+        const botaoConcluir = document.createElement('button');
 
-    botaoConcluir.classList.add('check-button');
-    botaoConcluir.innerText = 'Concluir';
+        botaoConcluir.classList.add('check-button');
+        botaoConcluir.innerText = 'Concluir';
 
-    botaoConcluir.addEventListener('click', concluirTarefa);
+        botaoConcluir.addEventListener('click', concluirTarefa);
 
-    return botaoConcluir;
-}
+        return botaoConcluir;
+    }
 
-const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target;
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target;
 
-    const tarefaCompleta = botaoConclui.parentElement;
+        const tarefaCompleta = botaoConclui.parentElement;
 
-    tarefaCompleta.classList.toggle('done');
-}
+        tarefaCompleta.classList.toggle('done');
+    }
+});
